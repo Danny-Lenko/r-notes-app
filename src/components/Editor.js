7124f67b90db34867076b1b2a1e0ac928cf3339b
DannyLenk <danylenko.14@gmail.com>
2022-04-27 13:39:46 +0300

    fixed sizes bugs

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -22,7 +22,7 @@ export default function Editor({ currentNote, updateNote }) {
                 generateMarkdownPreview={(markdown) =>
                     Promise.resolve(converter.makeHtml(markdown))
                 }
-                minEditorHeight={70}
+                minEditorHeight={64.753}
                 heightUnits="vh"
             />
         </main>
